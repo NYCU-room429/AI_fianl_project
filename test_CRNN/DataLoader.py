9ea3d0f36b6370f02673bb258c0b48f0dad48fe1
Lin-shao-an <brianlin.cs12@nycu.edu.tw>
2025-05-29 12:28:29 +0800

    Refactor code structure for improved readability and maintainability

--- test_CRNN/DataLoader.py
+++ test_CRNN/DataLoader.py
@@ -1,26 +1,34 @@
 from torch.utils.data import Dataset, DataLoader
 import torch
 import os, tqdm
-from utils import gen_melgram, get_all_class, extract_label_from_midi, get_rendered_stems
+from utils import (
+    gen_melgram,
+    get_all_class,
+    extract_label_from_midi,
+    get_rendered_stems,
+)
+
 
 def collect_all_path(root_path, is_test=False):
-    target_class = get_all_class('D:\\AI_fianl_project\\slakh-utils\\midi_inst_values\\general_midi_inst_0based.json')
+    target_class = get_all_class(
+        "C:\\Users\\linbr\\Desktop\\AI_fianl_project\\slakh-utils\\midi_inst_values\\general_midi_inst_0based.json"
+    )
     midi_files = []
     flac_files = []
 
-    for track in tqdm.tqdm(os.listdir(root_path), desc='Collecting all files\' path'):
+    for track in tqdm.tqdm(os.listdir(root_path), desc="Collecting all files' path"):
         track_path = os.path.join(root_path, track)
-        stems_dir = os.path.join(track_path, 'stems')
-        midi_dir =  os.path.join(track_path, 'MIDI')
-        metadata_path = os.path.join(track_path, 'metadata.yaml')
+        stems_dir = os.path.join(track_path, "stems")
+        midi_dir = os.path.join(track_path, "MIDI")
+        metadata_path = os.path.join(track_path, "metadata.yaml")
         rendered_stems = get_rendered_stems(metadata_path)
 
         if not os.path.isdir(track_path):
             continue
-        
-        midi_path = os.path.join(track_path, 'all_src.mid')
-        flac_path = os.path.join(track_path, 'mix.flac')
-        
+
+        midi_path = os.path.join(track_path, "all_src.mid")
+        flac_path = os.path.join(track_path, "mix.flac")
+
         if extract_label_from_midi(midi_path, target_class) is None:
             continue
 
@@ -29,8 +37,8 @@ def collect_all_path(root_path, is_test=False):
 
         if is_test:
             continue
-        flac_list = sorted([f for f in os.listdir(stems_dir) if f.endswith('.flac')])
-        midi_list = sorted([f for f in os.listdir(midi_dir) if f.endswith('.mid')])
+        flac_list = sorted([f for f in os.listdir(stems_dir) if f.endswith(".flac")])
+        midi_list = sorted([f for f in os.listdir(midi_dir) if f.endswith(".mid")])
 
         flac_basenames = [os.path.splitext(f)[0] for f in flac_list]
         midi_basenames = [os.path.splitext(f)[0] for f in midi_list]
@@ -39,8 +47,8 @@ def collect_all_path(root_path, is_test=False):
         for name in sorted(common_names):
             if name not in rendered_stems:
                 continue
-            midi_small_path = os.path.join(midi_dir, name + '.mid')
-            flac_small_path = os.path.join(stems_dir, name + '.flac')
+            midi_small_path = os.path.join(midi_dir, name + ".mid")
+            flac_small_path = os.path.join(stems_dir, name + ".flac")
             if extract_label_from_midi(midi_small_path, target_class) is None:
                 continue
             midi_files.append(midi_small_path)
@@ -50,11 +58,14 @@ def collect_all_path(root_path, is_test=False):
 
     return midi_files, flac_files
 
+
 class LoadDataset(Dataset):
     def __init__(self, midi_file_list, flac_file_list, is_train=False):
         self.midi_file_list = midi_file_list
         self.flac_file_list = flac_file_list
-        self.target_class = get_all_class('D:\\AI_fianl_project\\slakh-utils\\midi_inst_values\\general_midi_inst_0based.json')
+        self.target_class = get_all_class(
+            "C:\\Users\\linbr\\Desktop\\AI_fianl_project\\slakh-utils\\midi_inst_values\\general_midi_inst_0based.json"
+        )
         self.is_train = is_train
         # for midi, flac in tqdm.tqdm(zip(midi_file_list, flac_file_list), desc="init data"):
         #     mel = gen_melgram(flac)
@@ -68,7 +79,9 @@ class LoadDataset(Dataset):
         return len(self.midi_file_list)
 
     def __getitem__(self, idx):
-        mel = gen_melgram(self.flac_file_list[idx], is_train = self.is_train)  # numpy array
+        mel = gen_melgram(
+            self.flac_file_list[idx], is_train=self.is_train
+        )  # numpy array
         mel_mean = mel.mean()
         mel_std = mel.std()
         if mel_std > 0:
--- test_CRNN/test.py
+++ test_CRNN/test.py
@@ -6,7 +6,16 @@ from utils import gen_melgram, get_all_class, extract_label_from_midi
 from CRNN import CRNN
 from sklearn.metrics import f1_score, precision_score, recall_score
 
-def test(model_path, midi_file_list, flac_file_list, class_json, batch_size=1, device='cuda', num_tracks=3):
+
+def test(
+    model_path,
+    midi_file_list,
+    flac_file_list,
+    class_json,
+    batch_size=1,
+    device="cuda",
+    num_tracks=3,
+):
     midi_file_list = midi_file_list[:num_tracks]
     flac_file_list = flac_file_list[:num_tracks]
 
@@ -17,10 +26,12 @@ def test(model_path, midi_file_list, flac_file_list, class_json, batch_size=1, d
     model.eval()
 
     test_dataset = LoadDataset(midi_file_list, flac_file_list, is_train=False)
-    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    test_loader = torch.utils.data.DataLoader(
+        test_dataset, batch_size=batch_size, shuffle=False
+    )
 
     best_thresholds = np.load("best_thresholds.npy")
-    threshold = np.array([max(0.45, t) for t in  best_thresholds])
+    threshold = np.array([max(0.66, t) for t in best_thresholds])
     # threshold = np.array([max(0.45, t) for t in  [0.49, 0.1, 0.5700000000000001, 0.59, 0.31, 0.1, 0.49, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.51, 0.1, 0.45, 0.1, 0.1, 0.46, 0.1, 0.37, 0.5, 0.1, 0.5700000000000001, 0.22, 0.1, 0.1, 0.38, 0.36000000000000004, 0.4, 0.46, 0.38, 0.51, 0.44, 0.1, 0.1, 0.1, 0.1, 0.1, 0.12, 0.17, 0.1, 0.1, 0.1, 0.1, 0.35000000000000003, 0.21000000000000002, 0.45, 0.1, 0.1, 0.1, 0.1, 0.27, 0.35000000000000003, 0.1, 0.24000000000000002, 0.21000000000000002, 0.11, 0.17, 0.29000000000000004, 0.35000000000000003, 0.1, 0.11, 0.1, 0.1, 0.25, 0.1, 0.1, 0.1, 0.1, 0.25, 0.16, 0.1, 0.1, 0.52, 0.34, 0.53, 0.36000000000000004, 0.22, 0.1, 0.1, 0.39, 0.2, 0.39, 0.22, 0.1, 0.23, 0.1, 0.1, 0.1, 0.5700000000000001, 0.1, 0.1, 0.1, 0.1, 0.1, 0.31, 0.1, 0.14, 0.1, 0.31, 0.55, 0.26, 0.44, 0.47000000000000003, 0.2, 0.1, 0.5, 0.34, 0.1, 0.1, 0.1, 0.1, 0.35000000000000003, 0.1, 0.1, 0.13, 0.1, 0.18000000000000002, 0.1, 0.1, 0.31, 0.1, 0.13, 0.44, 0.1, 0.1, 0.1]])
 
     track_precisions = []
@@ -31,9 +42,13 @@ def test(model_path, midi_file_list, flac_file_list, class_json, batch_size=1, d
         for idx, (mel, label) in enumerate(test_loader):
             mel = mel.to(device)
             outputs = model(mel)
-            threshold_tensor = torch.tensor(threshold, dtype=outputs.dtype, device=outputs.device)
-            preds = (torch.sigmoid(outputs) > threshold_tensor).cpu().numpy() # (batch, 1000, num_classes)
-            label_np = label.numpy()                              # (batch, 1000, num_classes)
+            threshold_tensor = torch.tensor(
+                threshold, dtype=outputs.dtype, device=outputs.device
+            )
+            preds = (
+                (torch.sigmoid(outputs) > threshold_tensor).cpu().numpy()
+            )  # (batch, 1000, num_classes)
+            label_np = label.numpy()  # (batch, 1000, num_classes)
 
             # 只看有出現過的樂器（frame-wise OR）
             pred_inst_idx = np.where(preds[0].any(axis=0))[0]
@@ -46,10 +61,27 @@ def test(model_path, midi_file_list, flac_file_list, class_json, batch_size=1, d
             print("預測樂器名稱:", pred_inst)
 
             # 計算這個 track 的 precision/recall/f1 (micro)
-            track_f1 = f1_score(label_np.reshape(-1, len(instrument_class)), preds.reshape(-1, len(instrument_class)), average='micro', zero_division=0)
-            track_precision = precision_score(label_np.reshape(-1, len(instrument_class)), preds.reshape(-1, len(instrument_class)), average='micro', zero_division=0)
-            track_recall = recall_score(label_np.reshape(-1, len(instrument_class)), preds.reshape(-1, len(instrument_class)), average='micro', zero_division=0)
-            print(f"Track F1: {track_f1:.4f} | Precision: {track_precision:.4f} | Recall: {track_recall:.4f}")
+            track_f1 = f1_score(
+                label_np.reshape(-1, len(instrument_class)),
+                preds.reshape(-1, len(instrument_class)),
+                average="micro",
+                zero_division=0,
+            )
+            track_precision = precision_score(
+                label_np.reshape(-1, len(instrument_class)),
+                preds.reshape(-1, len(instrument_class)),
+                average="micro",
+                zero_division=0,
+            )
+            track_recall = recall_score(
+                label_np.reshape(-1, len(instrument_class)),
+                preds.reshape(-1, len(instrument_class)),
+                average="micro",
+                zero_division=0,
+            )
+            print(
+                f"Track F1: {track_f1:.4f} | Precision: {track_precision:.4f} | Recall: {track_recall:.4f}"
+            )
 
             track_f1s.append(track_f1)
             track_precisions.append(track_precision)
@@ -60,15 +92,19 @@ def test(model_path, midi_file_list, flac_file_list, class_json, batch_size=1, d
     print(f"平均 Precision: {np.mean(track_precisions):.4f}")
     print(f"平均 Recall: {np.mean(track_recalls):.4f}")
 
+
 if __name__ == "__main__":
-    test_midi_file_list, test_flac_file_list = collect_all_path('D:\\AI_fianl_project\\slakh2100_flac_redux\\slakh2100_flac_redux\\test', is_test=True)
+    test_midi_file_list, test_flac_file_list = collect_all_path(
+        "C:\\Users\\linbr\\Desktop\\AI_fianl_project\\slakh2100_flac_redux\\slakh2100_flac_redux\\test",
+        is_test=True,
+    )
 
     test(
-        model_path = 'D:\\AI_fianl_project\\best_model.pth',
-        midi_file_list = test_midi_file_list,
-        flac_file_list = test_flac_file_list,
-        class_json = 'D:\\AI_fianl_project\\slakh-utils\\midi_inst_values\\general_midi_inst_0based.json',
-        batch_size = 1,
-        device='cuda' if torch.cuda.is_available() else 'cpu',
-        num_tracks=132
+        model_path="C:\\Users\\linbr\\Desktop\\AI_fianl_project\\test_CRNN\\best_model.pth",
+        midi_file_list=test_midi_file_list,
+        flac_file_list=test_flac_file_list,
+        class_json="C:\\Users\\linbr\\Desktop\\AI_fianl_project\\slakh-utils\\midi_inst_values\\general_midi_inst_0based.json",
+        batch_size=1,
+        device="cuda" if torch.cuda.is_available() else "cpu",
+        num_tracks=132,
     )
